6a7386c350c2eb69b8dd5580556bd02296b7ce3e
wojst <wojtek4531@gmail.com>
2022-10-26 00:29:54 +0200

    Pierwszy commit

---
+++ src/CelsFahr/CelsFar.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CelsFar extends JFrame {
+    private JLabel LabelC;
+    private JLabel LabelWynikF;
+    private JTextField textField1;
+    private JButton obliczButton;
+    private JPanel Panel1;
+
+    double poZmianieF, tempC;
+
+    public static void main(String[] args) {
+        CelsFar p1 = new CelsFar();
+        p1.setVisible(true);
+    }
+
+    public CelsFar() {
+        super("Zmiana Celsjusz na Fahrenheit");
+        this.setContentPane(this.Panel1); // wyswietla na ekranie
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(400, 300);
+
+        obliczButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tempC = Double.parseDouble(textField1.getText());
+                poZmianieF = tempC * 9/5 + 32;
+                LabelWynikF.setText(LabelWynikF.getText() + " " + String.valueOf(poZmianieF));
+            }
+        });
+    }
+}
---
+++ src/ZadDom.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ZadDom extends JFrame {
+    private JPanel Panel1;
+    private JTextField textField1;
+    private JLabel wprowadzLabel2;
+    private JLabel wprowadzLabel1;
+    private JTextField textField2;
+    private JButton dodajButton;
+    private JButton odejmijButton;
+    private JButton pomnozButton;
+    private JButton podzielButton;
+    private JLabel wynikLabel;
+
+    double liczba1, liczba2;
+    double wynik;
+
+    public static void main(String[] args) {
+        ZadDom k1 = new ZadDom();
+        k1.setVisible(true);
+    }
+
+    public ZadDom() {
+        super("Uproszczony Kalkulator");
+        this.setContentPane(this.Panel1);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(400,300);
+        this.pack();
+
+        dodajButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                liczba1 = Double.parseDouble(textField1.getText());
+                liczba2 = Double.parseDouble(textField2.getText());
+                wynik = liczba1 + liczba2;
+                wynikLabel.setText("Wynik: " + String.valueOf(wynik));
+            }
+        });
+        odejmijButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                liczba1 = Double.parseDouble(textField1.getText());
+                liczba2 = Double.parseDouble(textField2.getText());
+                wynik = liczba1 - liczba2;
+                wynikLabel.setText("Wynik: " + String.valueOf(wynik));
+            }
+        });
+        pomnozButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                liczba1 = Double.parseDouble(textField1.getText());
+                liczba2 = Double.parseDouble(textField2.getText());
+                wynik = liczba1 * liczba2;
+                wynikLabel.setText("Wynik: " + String.valueOf(wynik));
+            }
+        });
+        podzielButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                liczba1 = Double.parseDouble(textField1.getText());
+                liczba2 = Double.parseDouble(textField2.getText());
+                wynik = liczba1 / liczba2;
+                wynikLabel.setText("Wynik: " + wynik);
+            }
+        });
+    }
+
+}
